4f4b002fbabe4ac2f1478f06e0e91fbe77b4b625
Andrea Gherardi <andrea@panthacorp.com>
2014-06-30 01:47:19 +1000

    Minor tweaks

--- app/models/MoistureModel.js
+++ app/models/MoistureModel.js
@@ -4,13 +4,8 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var MoistureSchema = new Schema({
-    time: { type: Number, required: true },
-    value: { type: Number, required: true }
+  time: { type: Number, required: true },
+  value: { type: Number, required: true }
 });
-
-// MoistureLevel.virtual('date')
-//   .get(function(){
-//     return this._id.getTimestamp();
-//   });
 
 var MoistureModel = mongoose.model('MoistureLevel', MoistureSchema);
